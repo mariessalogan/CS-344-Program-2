69334706c59cc531923b21f579024a05b80b4b33
mariessalogan <38187604+mariessalogan@users.noreply.github.com>
2019-05-08 19:08:25 -0700

    Update loganma.adventure.c

--- loganma.adventure.c
+++ loganma.adventure.c
@@ -12,6 +12,7 @@ Sources: TA office hours, stack overflow, man, geeks for geeks
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
 #include <dirent.h>
 
 //Create the room struct
@@ -82,13 +83,50 @@ char *getDirectory()
     return mostCurrent->d_name;
 }
  
+//MUTEX TIME FUNCTION
+pthread_t tid[2]; 
+int counter; 
+pthread_mutex_t lock; 
+  
+void* timeCheck() 
+{ 
+	//LOCK THE THREAD
+	//MUTEX RIGHT HERE TA'S! I HOPE THIS IS EASIER TO SPOT :)
+    pthread_mutex_lock(&lock); 
+  
+    char timeStamp[200];
+    time_t t;
+    struct tm *tempTime;
+    //SET THE TIME AND CHECK THAT TEMPTIME ACTUALLY HAS A VALUE
+    t = time(NULL);
+    tempTime = localtime(&t);
+    if(tempTime == NULL)
+    {
+    	printf("Error computing time\n");
+    	return NULL;
+    }
+  	//format should be  time(1:03), weekay(word), month(word), date(#), year(#)
+  	strftime(timeStamp, sizeof(timeStamp), "%l:%M%P, %A, %B %d, %Y",tempTime);
+ 
+  	printf("\n%s\n", timeStamp);
+  	//PRINT TO A FILE NOW
+  	FILE *timeFile;
+  	timeFile = fopen("currentTime.txt", "w+");
+  	if(timeFile == NULL)
+        printf("Error, file couldn't open");
+      //Print time to file
+      fprintf(timeFile, timeStamp);
+      fclose(timeFile);
 
+   	//UNLOCK THE THREAD 
+  	//MORE MUTEX HERE TA'S! :D
+    pthread_mutex_unlock(&lock); 
+    return NULL; 
+}
 
-
-//Main function
-int main()
+//GAME FUNCTION
+void *playGame()
 {
-
 	//get directory
 	char *dirName;
     char roomFileName[25];
@@ -107,8 +145,6 @@ int main()
         return 0; 
     } 
  	strcat(roomFileName, "/");
-
-
  	int i =0;
  	int j = 0;
  	de = readdir(dr);
@@ -125,7 +161,7 @@ int main()
         }
     	j++;
     }
-  
+  	
     closedir(dr);  
     //Now start copying rooms into the room array
 	//room array structure
@@ -227,6 +263,16 @@ int main()
 			scanf("%s", whereTo);
 			for(j = 0; j <currentRoom->connections; j++)
 			{
+				if(! strcmp(whereTo, "time"))
+				{
+					int error;
+					error = pthread_create(&(tid[1]), NULL, &timeCheck, NULL); 
+     				if (error != 0) 
+        				printf("\nThread can't be created :[%s]", strerror(error)); 
+   				    pthread_join(tid[1], NULL); 
+					printf("\n");
+					break;
+				}
 				//make sure that it is a valid connection
 				if(! strcmp(whereTo, currentRoom->connectArray[j]))
 				{
@@ -245,7 +291,7 @@ int main()
 				}
 			}
 			//let the user know it's invalid
-			if(checkValid !=0)
+			if(checkValid !=0 && strcmp(whereTo, "time") != 0)
 			{
 				printf("\nHUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
 			}
@@ -269,6 +315,27 @@ int main()
 		printf("GAME OVER! YOU TOOK TOO LONG STORMING THE CASTLE!\n\nDon't do so bad next time\n");
 		return 0;
 	}
+}
+//Main function
+int main()
+{
+	//error will be a flag that the thread wasn't built
+	int error;
+	//initiate the thread MUTEX LOCK (HERE'S MUTEX TOO :D)
+    if (pthread_mutex_init(&lock, NULL) != 0) 
+    { 
+        printf("\n mutex init has failed\n"); 
+        return 1; 
+    } 
+    //check for errors
+	error = pthread_create(&(tid[0]), NULL, &playGame, NULL); 
+	if (error != 0) 
+ 		printf("\nThread can't be created :[%s]", strerror(error));
+ 	//Start the thread with the playgame function 
+   	pthread_join(tid[0], NULL); 
+   	//destroy the thread locks
+    pthread_mutex_destroy(&lock); 
+	
 
 	return 0;
 }
